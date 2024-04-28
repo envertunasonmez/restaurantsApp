d18c8382a876709162f24d5c55636a4a06aa4820
envertunasonmez <etsonmez01@gmail.com>
2024-04-28 21:10:07 +0300

    finish  - login screen created

--- lib/helper/bottom_sheet/modal_bottom_sheet.dart
+++ lib/helper/bottom_sheet/modal_bottom_sheet.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:get/get.dart';
 import 'package:restaurant_app/controller/auth/auth_controller.dart';
+import 'package:restaurant_app/core/constant/color.dart';
 import 'package:restaurant_app/core/extension/num_x.dart';
 import 'package:restaurant_app/widget_modal/button/button.dart';
 import '../../core/constant/radius.dart';
@@ -10,7 +12,7 @@ import '../../widget_modal/bottom_sheet/handler/handler.dart';
 import '../../widget_modal/input_field/input_field.dart';
 
 class AppBottomSheet {
-  static void showAuthBottomSheet() {
+  static void showCreateAccountBottomSheet() {
     showModalBottomSheet(
       context: Get.context!,
       isScrollControlled: true,
@@ -91,6 +93,112 @@ class AppBottomSheet {
                             padding: 8.w.pH,
                             child: AppButton(
                               text: 'Registration',
+                              onTap: () {},
+                              isInActive: true,
+                            ),
+                          ),
+                        ],
+                      ))
+                    ],
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+
+  static void showLogInBottomSheet() {
+    showModalBottomSheet(
+      context: Get.context!,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (context) => Container(
+        height: 76.h,
+        decoration: BoxDecoration(
+          color: Get.theme.colorScheme.secondary,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(
+              AppRadius.bottomSheetRadius,
+            ),
+            topRight: Radius.circular(
+              AppRadius.bottomSheetRadius,
+            ),
+          ),
+        ),
+        child: GetBuilder<AuthController>(
+            id: 'authController',
+            builder: (ac) {
+              return Center(
+                child: SizedBox(
+                  width: 80.w,
+                  child: Column(
+                    children: [
+                      2.yh,
+                      const BottomSheetHandler(),
+                      4.yh,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          BottomSheetCategory(
+                            text: 'Create Account',
+                            onTap: () {},
+                            isActive: false,
+                          ),
+                          BottomSheetCategory(
+                            text: 'Log In',
+                            onTap: () {},
+                            isActive: true,
+                          )
+                        ],
+                      ),
+                      4.yh,
+                      Form(
+                          child: Column(
+                        children: [
+                          AppInputField(
+                            controller: ac.emailTextController,
+                            title: 'Email address',
+                            hint: 'example@gmail.com',
+                            validator: FormBuilderValidators.compose([
+                              FormBuilderValidators.required(),
+                              FormBuilderValidators.email(),
+                            ]),
+                          ),
+                          2.yh,
+                          AppInputField(
+                            controller: ac.passwordTextController,
+                            title: 'Password',
+                            hint: '***** ****** *****',
+                            validator: FormBuilderValidators.compose([
+                              FormBuilderValidators.required(),
+                              FormBuilderValidators.minLength(6),
+                            ]),
+                          ),
+                          2.yh,
+                          GestureDetector(
+                            onTap: () {
+                              print('tıklandı');
+                            },
+                            child: Padding(
+                              padding: 160.pLeft,
+                              child: Container(
+                                child: Text(
+                                  'Forgot Password?',
+                                  style:
+                                      Get.theme.textTheme.labelMedium!.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: ColorConstants.primary,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          4.yh,
+                          Padding(
+                            padding: 8.w.pH,
+                            child: AppButton(
+                              text: 'Login',
                               onTap: () {},
                               isInActive: true,
                             ),
